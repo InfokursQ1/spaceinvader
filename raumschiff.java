2b4f9673d97757b3c91a44e30a450de732191597
infoQ1-mesih <infokursq1@gmail.com>
2016-12-04 20:15:16 +0100

    mesihs commit
    raumschiff hinzugefügt

---
+++ raumschiff.java
@@ -0,0 +1,21 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import java.util.*;
+import greenfoot.*;
+import java.awt.Color;
+
+/**
+ * //raumschiff actor
+ */
+public class raumschiff extends Actor
+{
+
+    /**
+     * public void links() {
+     * 
+     * //hiier kommt der code rein
+     * }
+     */
+    public void act()
+    {
+    }
+}
