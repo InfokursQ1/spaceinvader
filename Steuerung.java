155dd688abbe4c75361a3a08419cfd84ddab7083
infoQ1-enes <enesatmaca27@gmail.com>
2016-12-06 12:34:17 +0100

    Add files via upload
    6.12. Doppelstunde
    Raumschiff mit Steurung und Bullet
    Bullets programmiert

--- Steuerung.java
+++ Steuerung.java
@@ -5,19 +5,34 @@ import java.awt.Color;
 
 public class Steuerung extends Actor
 {
+    World meineWelt;
     MouseInfo MI;
     raumschiff Enterprise = new raumschiff();
+    Bullet meineKugel;
     public void act()
     {
         MI=Greenfoot.getMouseInfo();
         if (MI!=null)
         {
             Enterprise.setLocation(MI.getX(),363);
-        }
+            if (MI.getButton()==1)
+            {
+                shoot();
+            }
+        } 
     }
     public void addedToWorld(World w)
     {
         w.addObject(Enterprise,300,363);
+        meineWelt=w;
     }
+    public void shoot()
+    {
+      meineKugel = new Bullet();
+      meineWelt.addObject(meineKugel,Enterprise.getX(),363);
+      
+      
+    }
+    
 } 
 
