8b158bf9ac229e0ad1efde6e005942381309d8d7
infoQ1-enes <enesatmaca27@gmail.com>
2016-12-06 11:12:02 +0100

    Add files via upload

--- raumschiff.java
+++ raumschiff.java
@@ -1,40 +1,11 @@
-// WARNING: This file is auto-generated and any changes to it will be overwritten
 import java.util.*;
 import greenfoot.*;
 import java.awt.Color;
 
-/**
- * //raumschiff actor
- */
 public class raumschiff extends Actor
 {
-
-    /**
-     * 
-     */
-    public raumschiff()
+  public raumschiff()
     {
         setImage("Raumschiff.png");
-    }
-
-    /**
-     * public void links() {
-     * 
-     * //hier kommt der code rein
-     * 
-     * //code fürn raumschiff
-     * //eine zeile
-     * 
-     * }
-     */
-    public void act()
-    {
-    }
-
-    /**
-     * 
-     */
-    public void addedToWorld()
-    {
     }
 }
