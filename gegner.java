5bb3bf6a4578becc9a11738d5dfa9b202021135a
infoQ1-enes <enesatmaca27@gmail.com>
2016-12-19 15:41:22 +0100

    Add files via upload
    Gegnerbewegung
    Schussgeschwindigkeit

--- gegner.java
+++ gegner.java
@@ -4,9 +4,30 @@ import java.awt.Color;
 
 public class gegner extends Actor
 {
-  double lebenspunkte=1;   
+  double lebenspunkte=1;
+  int Zähler = 80;
+  int z = 10;
+  int dy=1;
   public gegner()
     {
         setImage("EinInvader.png");
     }
+    public void act()
+  {
+         z--;
+        if (z==0)
+        {
+        z=10;
+       
+        setLocation(getX()+dy,getY());
+        Zähler--;
+    
+        if(Zähler==0)   
+        {
+            Zähler=80;
+            dy=-dy;
+            setLocation(getX(),getY()+5);
+        }    
+       }
+  }  
 }
