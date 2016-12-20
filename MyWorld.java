9e59117519b71d1c676713f873147403fc549d39
infoQ1-mesih <infokursq1@gmail.com>
2016-12-20 12:26:22 +0100

    Add files via upload
    geschwindigkeit

--- MyWorld.java
+++ MyWorld.java
@@ -2,14 +2,20 @@ import java.util.*;
 import greenfoot.*;
 import java.awt.Color;
 
+/**
+ * 
+ */
 public class MyWorld extends World
 {
    Steuerung meineSteuerung= new Steuerung();
-    
-   public MyWorld()
-   {
+    /**
+     * Constructor for objects of class MyWorld.
+     */
+    public MyWorld()
+    {   
         super(800, 600, 1);
+        Greenfoot.setSpeed(100);
         setBackground("Mars.png");
         addObject(meineSteuerung,0,0);
-   }//na wie gehts
+    }
 }
