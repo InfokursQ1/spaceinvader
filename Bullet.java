b2e806046076e7746c4075e292c18d3d8b337721
infoQ1-mesih <infokursq1@gmail.com>
2016-12-20 11:33:51 +0100

    Add files via upload
    bullettreffer richtig

--- Bullet.java
+++ Bullet.java
@@ -3,21 +3,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Bullet extends Actor
 {
-    int z=  10;
     public void act() 
     {
-          z--;
-        if (z==0)
-        {
-        z=50;
-       
         setLocation(getX(),getY()-15);
         if (getY()==0) {
             getWorld().removeObject(this);
+            
+         
         
         
+        }else {
+        gegner g=(gegner)(getOneIntersectingObject(gegner.class));
+            getWorld().removeObject(g);
+            
         }
     }   
     
-}
 }
