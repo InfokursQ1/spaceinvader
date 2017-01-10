e0a59d97e4bf0ce678b71706782e1715fc6b9488
infoQ1-mesih <infokursq1@gmail.com>
2017-01-10 12:33:50 +0100

    Update gegner.java

--- gegner.java
+++ gegner.java
@@ -5,8 +5,8 @@ import java.awt.Color;
 public class gegner extends Actor
 {
   double lebenspunkte=1;
-  int Zähler = 480;
-  int z = 200;
+  int Zähler = 80;
+  int z = 600;
   int dy=1;
   public gegner()
     {
@@ -17,14 +17,14 @@ public class gegner extends Actor
          z--;
         if (z==0)
         {
-        z=200;
+        z=600;
        
         setLocation(getX()+dy,getY());
         Zähler--;
     
         if(Zähler==0)   
         {
-            Zähler=480;
+            Zähler=80;
             dy=-dy;
             setLocation(getX(),getY()+5);
         }    
